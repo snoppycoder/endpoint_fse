43c9f7ec9e65facdde8d22724675a595e22384aa
Milki Legesse <nitfanono@gmail.com>
2025-01-10 12:23:03 +0300

    Update main.ts

--- app/src/main.ts
+++ app/src/main.ts
@@ -4,7 +4,18 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe())
+
+  // Enable CORS
+  app.enableCors({
+    origin: '*', // Allows requests from any origin (you can restrict this for security)
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Allowed HTTP methods
+    credentials: true, // Allows cookies and credentials to be sent with requests
+  });
+
+  // Use global validation pipe
+  app.useGlobalPipes(new ValidationPipe());
+
   await app.listen(process.env.PORT ?? 3000);
 }
+
 bootstrap();
